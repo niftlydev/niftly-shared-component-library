90bef4a697917f87a852f808e0fe807299da4ef9
Caleb Kliewer <cfkliewer@gmail.com>
2021-01-31 16:06:17 -0600

    updated listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,31 +30,7 @@ const IndexPage = () => {
       <title>Home Page</title>
 
         <Layout>
-          <AboutUs>
-            <Box m="5%" flex="1 0 25%" backgroundColor="rgba(50,50,50, .4)">
-              <Center>
-                <Image w="30%" objectFit="cover" src={ImageOne}/>
-              </Center>
-            </Box>
-            <Box flex="1 0 25%" p="10%" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-                <Text fontSize="24px" paddingBottom="5%">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Text>
-                <Text fontSize="24px">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Text>
-            </Box>
-            <Box flex="1 0 25%" display="flex" flexDirection="row" justifyContent="space-between" p="5%">
-              <Box p="10px" flex="0 1 45%">
-                <InfoBox info={infobox} border={true}/>
-              </Box>
-              <Box p="10px" flex="0 1 45%">
-                <InfoBox info={infobox} border={true}/>
-              </Box>
-            </Box>
-            <Box flex="1 0 25%" display="flex" flexDirection="row" p="10%">
-              <HighlightBox highlights={highlightBoxFactory()} withBorder={true}/>
-            </Box>
-          </AboutUs>
+          <Listing listing={ListingFactory()}/>
         </Layout>
     </main> 
   )
