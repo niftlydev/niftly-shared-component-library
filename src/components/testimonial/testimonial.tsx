c0708e3d98396e90f93f33bf86ea6edc3a0bd9c0
Caleb Kliewer <cfkliewer@gmail.com>
2021-02-03 19:34:10 -0600

    created about page

--- src/components/testimonial/testimonial.tsx
+++ src/components/testimonial/testimonial.tsx
@@ -1,11 +1,16 @@
 import { Box, Heading, Text } from "@chakra-ui/react"
 import React from "react"
 
-export const Testimonial = ({title, description}) => {
+type TestimonialProps = {
+    title: string,
+    description: string
+}
+
+export const Testimonial = ({title, description}: TestimonialProps) => {
     return (
         <Box display="flex" flexDirection="column" alignItems="center" justifyContent="space-between">
             <Heading size="lg" paddingBottom="5%">{title}</Heading>
-            <Text fontSize="24px">{description}</Text>
+            <Text fontSize="24px" whiteSpace="pre-line">{description}</Text>
         </Box>
     )
 }
