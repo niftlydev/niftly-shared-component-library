166cafe15b55eca69d4f3f100f924bdfbee0d958
Caleb Kliewer <cfkliewer@gmail.com>
2021-03-09 21:22:04 -0600

    updated infobox

--- src/utils/footer/column-factory.ts
+++ src/utils/footer/column-factory.ts
@@ -1,33 +1,65 @@
-import * as React from "react";
-import { Column, ColumnType, ColumnDetailsInfo, ColumnDetailsLinks, ColumnDetailsSocial } from "../../components/footer/footer";
+import * as React from "react";
 
+import { Column, ColumnType, ColumnDetailsInfo, ColumnDetailsLinks, ColumnDetailsSocial } from "@bit/niftly.shared.footer";
 
-const columnFactory = (footerInfo?, footerLinks?, footerSocial?): Array<Column> => {
-     var columns = new Array<Column>();
-  
-     if (footerInfo !== null){
-          footerInfo.forEach(e => {
-               columns.push(new Column(ColumnType.Info, "Info", e));
-          });
-     }
+
 
-     if (footerLinks !== null){
-          var links = new Array<ColumnDetailsLinks>();
+
 
-          footerLinks.forEach(footerLink => {
-               links.push(footerLink);
-          });
-     
-          columns.push(new Column(ColumnType.Links, "Links", null, links));
-     }
-     
-     if (footerSocial !== null){
-          footerSocial.forEach(e => {
-               columns.push(new Column(ColumnType.Social, "Social", null, null, e));
-          });
-     }
+const columnFactory = (footerInfo?, footerLinks?, footerSocial?): Array<Column> => {
 
-     return columns;
-}
+     var columns = new Array<Column>();
+
+  
+
+     if (footerInfo !== null){
+
+          footerInfo.forEach(e => {
+
+               columns.push(new Column(ColumnType.Info, "Info", e));
+
+          });
+
+     }
+
+
+
+     if (footerLinks !== null){
+
+          var links = new Array<ColumnDetailsLinks>();
+
+
+
+          footerLinks.forEach(footerLink => {
+
+               links.push(footerLink);
+
+          });
+
+     
+
+          columns.push(new Column(ColumnType.Links, "Links", null, links));
+
+     }
+
+     
+
+     if (footerSocial !== null){
+
+          footerSocial.forEach(e => {
+
+               columns.push(new Column(ColumnType.Social, "Social", null, null, e));
+
+          });
+
+     }
+
+
+
+     return columns;
+
+}
+
+
 
 export default columnFactory;
--- src/utils/form/form-factory.ts
+++ src/utils/form/form-factory.ts
@@ -1,26 +1,51 @@
-import {NetlifyFormItem, InputSize, InputType, INetlifyFormItem} from '../../components/form/netlify-form';
+import {NetlifyFormItem, InputSize, InputType, INetlifyFormItem} from '@bit/niftly.shared.form';
 
-export interface IFormFactoryProps {
-    formItems: Array<INetlifyFormItem>;
-}
+
 
-export const formFactory = () => {
-    let forms: Array<INetlifyFormItem> = new Array<INetlifyFormItem>();
+export interface IFormFactoryProps {
 
-    let first: INetlifyFormItem = new NetlifyFormItem("label 1", InputType.Text, InputSize.small, "label 1", true);
-    let second: INetlifyFormItem = new NetlifyFormItem("label 2", InputType.Text, InputSize.small, "label 2");
-    let third: INetlifyFormItem = new NetlifyFormItem("label 3", InputType.Text, InputSize.small, "label 3");
-    let fourth: INetlifyFormItem = new NetlifyFormItem("label 4", InputType.Email, InputSize.medium, "label 4");
-    let fifth: INetlifyFormItem = new NetlifyFormItem("label 5", InputType.Password, InputSize.medium, "label 5");
-    let sixth: INetlifyFormItem = new NetlifyFormItem("label 6", InputType.Text, InputSize.large, "label 6");
-    let seven: INetlifyFormItem = new NetlifyFormItem("Submit", InputType.Submit, InputSize.large, "");
+    formItems: Array<INetlifyFormItem>;
 
-    forms.push(first);
-    forms.push(second);
-    forms.push(third);
-    forms.push(fourth);
-    forms.push(fifth);
-    forms.push(sixth);
-    forms.push(seven);
-    return forms;
+}
+
+
+
+export const formFactory = () => {
+
+    let forms: Array<INetlifyFormItem> = new Array<INetlifyFormItem>();
+
+
+
+    let first: INetlifyFormItem = new NetlifyFormItem("label 1", InputType.Text, InputSize.small, "label 1", true);
+
+    let second: INetlifyFormItem = new NetlifyFormItem("label 2", InputType.Text, InputSize.small, "label 2");
+
+    let third: INetlifyFormItem = new NetlifyFormItem("label 3", InputType.Text, InputSize.small, "label 3");
+
+    let fourth: INetlifyFormItem = new NetlifyFormItem("label 4", InputType.Email, InputSize.medium, "label 4");
+
+    let fifth: INetlifyFormItem = new NetlifyFormItem("label 5", InputType.Password, InputSize.medium, "label 5");
+
+    let sixth: INetlifyFormItem = new NetlifyFormItem("label 6", InputType.Text, InputSize.large, "label 6");
+
+    let seven: INetlifyFormItem = new NetlifyFormItem("Submit", InputType.Submit, InputSize.large, "");
+
+
+
+    forms.push(first);
+
+    forms.push(second);
+
+    forms.push(third);
+
+    forms.push(fourth);
+
+    forms.push(fifth);
+
+    forms.push(sixth);
+
+    forms.push(seven);
+
+    return forms;
+
 }
